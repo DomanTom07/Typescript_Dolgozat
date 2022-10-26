b8a5faae8dd9412e3d857d8a96a926516c49a018
DomanTom07 <tamasdoman7@citromail.hu>
2022-10-26 19:40:18 +0200

    Lehetséges hibákat elhárító függvények hozzáadva

--- src/app.js
+++ src/app.js
@@ -14,12 +14,62 @@ calcButton.addEventListener('click', function () {
     var angle = document.querySelector('#angle');
     var sideNum = Number(side.value);
     var angleNum = Number(angle.value);
-    var radius = calcRadius(sideNum, angleNum);
-    result.textContent = "A beírható kör sugara: " + String(radius);
+    if (checkErrors(sideNum, angleNum)) {
+        var radius = calcRadius(sideNum, angleNum);
+        result.textContent = "A beírható kör sugara: " + String(radius);
+    }
     side.value = "";
     angle.value = "";
 });
 function calcRadius(side, angle) {
     var angleRad = angle * Math.PI / 180;
     return side / 2 * Math.sin(angleRad);
+}
+function checkErrors(side, angle) {
+    if (!checkSideGiven(side))
+        return false;
+    if (!checkSideNum(side))
+        return false;
+    if (!checkAngleGiven(angle))
+        return false;
+    if (!checkAngleNum(angle))
+        return false;
+    if (!checkAngleValue(angle))
+        return false;
+    return true;
+}
+function checkSideGiven(side) {
+    if (side == "") {
+        alert("Hiba! Kérem töltse ki mindkét mezőt!");
+        return false;
+    }
+    return true;
+}
+function checkSideNum(side) {
+    if (isNaN(side)) {
+        alert("Hibás adat! Kérem számot adjon meg!");
+        return false;
+    }
+    return true;
+}
+function checkAngleGiven(angle) {
+    if (angle == "") {
+        alert("Hiba! Kérem töltse ki mindkét mezőt!");
+        return false;
+    }
+    return true;
+}
+function checkAngleNum(angle) {
+    if (isNaN(angle)) {
+        alert("Hibás adat! Kérem számot adjon meg!");
+        return false;
+    }
+    return true;
+}
+function checkAngleValue(angle) {
+    if (angle >= 180) {
+        alert("Hibás adat! Rombuszban nem lehet ekkora szög!");
+        return false;
+    }
+    return true;
 }
--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,10 @@ calcButton.addEventListener('click', () => {
     const angle = document.querySelector('#angle') as HTMLInputElement;
     let sideNum:number = Number(side.value);
     let angleNum:number = Number(angle.value);
-    let radius = calcRadius(sideNum, angleNum);
-    result.textContent = "A beírható kör sugara: " + String(radius);
+    if (checkErrors(sideNum, angleNum)) {
+        let radius = calcRadius(sideNum, angleNum);
+        result.textContent = "A beírható kör sugara: " + String(radius);
+    }
     side.value = "";
     angle.value = "";
 });
@@ -25,4 +27,47 @@ calcButton.addEventListener('click', () => {
 function calcRadius(side:number, angle:number) {
     let angleRad:number = angle * Math.PI / 180;
     return side / 2 * Math.sin(angleRad);
+}
+function checkErrors(side, angle) {
+    if (!checkSideGiven(side)) return false;
+    if (!checkSideNum(side)) return false;
+    if (!checkAngleGiven(angle)) return false;
+    if (!checkAngleNum(angle)) return false;
+    if (!checkAngleValue(angle)) return false;
+    return true;
+}
+function checkSideGiven(side) {
+    if (side == "") {
+        alert("Hiba! Kérem töltse ki mindkét mezőt!");
+        return false;
+    }
+    return true;
+}
+function checkSideNum(side) {
+    if (isNaN(side)) {
+        alert("Hibás adat! Kérem számot adjon meg!");
+        return false;
+    }
+    return true;
+}
+function checkAngleGiven(angle) {
+    if (angle == "") {
+        alert("Hiba! Kérem töltse ki mindkét mezőt!");
+        return false;
+    }
+    return true;
+}
+function checkAngleNum(angle) {
+    if (isNaN(angle)) {
+        alert("Hibás adat! Kérem számot adjon meg!");
+        return false;
+    }
+    return true;
+}
+function checkAngleValue(angle) {
+    if (angle >= 180) {
+        alert("Hibás adat! Rombuszban nem lehet ekkora szög!");
+        return false;
+    }
+    return true;
 }
